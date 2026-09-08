243343ad6918d26512de6c022e30ba7f25d1169d
noamazar84-commits <noamazar84@gmail.com>
2026-09-09 00:10:12 +0300

    Update metawebookController.ts

--- src/controllers/metawebookController.ts
+++ src/controllers/metawebookController.ts
@@ -1,2 +1,6 @@
-export const metawebookController = null;
-export type MetawebookEvent = unknown;
+export const handleMetawebookVerify = (req: any, res: any) => {
+  return res.status(200).send(req.query["hub.challenge"] || "OK");
+};
+
+export const metawebookController = {};
+export type MetawebookEvent = any;
